3dece398792a465b10c74341a0122abfc415eab1
masoud mahmoodzadeh <mahmoodzadeh.masoud@gmail.com>
2019-10-30 06:01:16 +0330

    init MutableLiveData in Fragment A

--- app/src/main/java/com/masoud/mediatorlivedata/FragmentA.java
+++ app/src/main/java/com/masoud/mediatorlivedata/FragmentA.java
@@ -1,5 +1,6 @@
 package com.masoud.mediatorlivedata;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,24 @@ import android.widget.SeekBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.MutableLiveData;
 
 public class FragmentA extends Fragment {
 
     private SeekBar seekBar_A;
+    private MutableLiveData<Integer> liv_progress;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        liv_progress = new MutableLiveData<>();
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a , container , false);
+        return inflater.inflate(R.layout.fragment_a, container, false);
     }
 
     @Override
@@ -27,9 +37,21 @@ public class FragmentA extends Fragment {
         findView(view);
     }
 
-    private void findView(View rootView){
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        liv_progress.setValue(seekBar_A.getProgress());
+    }
+
+    private void findView(View rootView) {
 
         seekBar_A = rootView.findViewById(R.id.seekBar_A);
+    }
+
+    public MutableLiveData getLiveData(){
+
+        return liv_progress;
     }
 
 
