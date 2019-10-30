cee3e8173bfcc39236acd16dea08048daf9f62a3
masoud mahmoodzadeh <mahmoodzadeh.masoud@gmail.com>
2019-10-30 05:54:55 +0330

    create Fragment A

---
+++ app/src/main/java/com/masoud/mediatorlivedata/FragmentA.java
@@ -0,0 +1,36 @@
+package com.masoud.mediatorlivedata;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SeekBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentA extends Fragment {
+
+    private SeekBar seekBar_A;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a , container , false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        findView(view);
+    }
+
+    private void findView(View rootView){
+
+        seekBar_A = rootView.findViewById(R.id.seekBar_A);
+    }
+
+    
+}
